e2509ceb7f7a55d1f41de724c71a8e0e9f70eb84
Jengwen <jenalexsarah@gmail.com>
2020-06-26 14:09:24 -0400

    attempt delpoy

--- flightplan.js
+++ flightplan.js
@@ -1,5 +1,6 @@
 var plan = require('flightplan');
 
+
 var appName = 'healthapp';
 var username = 'deploy';
 var startFile = 'server.js';
@@ -32,8 +33,8 @@ plan.target('production', [
 // run commands on localhost
 plan.local(function(local) {
   // uncomment these if you need to run a build on your machine first
-  // local.log('Run build');
-  // local.exec('gulp build');
+   //local.log('Run build');
+  //local.exec('gulp build');
 
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
